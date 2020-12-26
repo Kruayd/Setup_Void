f83c2aae5543962a35f5af65fc4c418690f67558
Kruayd <kruayd@ASDEX-Upgrade.homenet.telecomitalia.it>
2020-12-26 16:31:47 +0100

    fixed roating functions

--- usr/local/bin/autorotate.sh
+++ usr/local/bin/autorotate.sh
@@ -1,54 +1,38 @@
 #!/bin/bash
 
-export DISPLAY=':0'
-export XAUTHORITY="/home/kruayd/.Xauthority"
-
 rotate="/usr/local/bin/rotate-all.sh"
-rotatepen="/usr/loca/bin/rotatepen.sh"
-rotateer="/usr/loca/bin/rotateer.sh"
 current="none"
 lastr="none"
-pen="none"
-eraser="none"
 
 accelx=$(find /sys/devices/ -iname *accel_x_raw | grep HID-SENSOR-200073.12)
 accely=$(find /sys/devices/ -iname *accel_y_raw | grep HID-SENSOR-200073.12)
 
 while true; do
 
-	sleep 0.1
+        sleep 0.1
 
-	commandx=$(cat $accelx)
-	commandy=$(cat $accely)
+        commandx=$(cat $accelx)
+        commandy=$(cat $accely)
 
-	if [[ $commandx -gt 600000 ]]
-	then
-		current="ccw"
-	elif [[ $commandx -lt -600000 ]]
-	then
-		current="cw"
-	elif [[ $commandy -gt 750000 ]]
-	then
-		current="half"
-	elif [[ $commandy -lt -750000 ]]
-	then
-		current="none"
-	fi
+        if [[ $commandx -gt 600000 ]]
+        then
+                current="ccw"
+        elif [[ $commandx -lt -600000 ]]
+        then
+                current="cw"
+        elif [[ $commandy -gt 750000 ]]
+        then
+                current="half"
+        elif [[ $commandy -lt -750000 ]]
+        then
+                current="none"
+        fi
 
-	if [[ $pen == "none"]]
-	then
-		xinput list | grep "CUST0000:00 04F3:2A4B Pen (0)" && pen="detected" && $rotatepen $current
-	fi
-	if [[ $eraser == "none"]]
-	then
-		xinput list | grep "CUST0000:00 04F3:2A4B Eraser (0)" && eraser="detected" && $rotateer $current
-	fi
-
-	if [[ $lastr != $current ]]
-	then
-		echo "auto-rotating $current"
-		$rotate $current
-		lastr=$current
-	fi
+        if [[ $lastr != $current ]]
+        then
+                echo "auto-rotating $current"
+                $rotate $current
+                lastr=$current
+        fi
 
 done
--- usr/local/bin/rotate-all.sh
+++ usr/local/bin/rotate-all.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+#rotate-all.sh
+
+export DISPLAY=':0'
+export XAUTHORITY="/home/kruayd/.Xauthority" #what if another user?
+
 if [[ $# -ne 1 ]]
 then
-	echo "You need to specify which way to rotate."
-	echo "EX: none, half (180 degrees), wc, ccw"
-	exit 1
+        echo "You need to specify which way to rotate."
+        echo "EX: none, half (180 degrees), wc, ccw"
+        exit 1
 fi
 
-DEFAULTCTM="1 0 0 0 1 0 0 0 1"	#default Coordinate Transformation Matrix
+DEFAULTCTM="1 0 0 0 1 0 0 0 1"  #default Coordinate Transformation Matrix
 leftr="0 -1 1 1 0 0 0 0 1"
 rightr="0 1 0 -1 0 1 0 0 1"
 upsider="-1 0 1 0 -1 1 0 0 1"
@@ -17,20 +22,20 @@ rotation=$1
 
 if [[ $rotation == "ccw" ]]
 then
-	CTM=$leftr
-	xrandr_action="left"
+        CTM=$leftr
+        xrandr_action="left"
 elif [[ $rotation == "cw" ]]
 then
-	CTM=$rightr
-	xrandr_action="right"
+        CTM=$rightr
+        xrandr_action="right"
 elif [[ $rotation == "none" ]]
 then
-	CTM=$DEFAULTCTM
-	xrandr_action="normal"
+        CTM=$DEFAULTCTM
+        xrandr_action="normal"
 elif [[ $rotation == "half" ]]
 then
-	CTM=$upsider
-	xrandr_action="inverted"
+        CTM=$upsider
+        xrandr_action="inverted"
 fi
 
 
