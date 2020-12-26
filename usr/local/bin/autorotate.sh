b321e28a975c1d61cf565386226cf3265bdc536b
Kruayd <kruayd@ASDEX-Upgrade.homenet.telecomitalia.it>
2020-12-26 22:37:34 +0100

    updated rotattion functions

--- usr/local/bin/autorotate.sh
+++ usr/local/bin/autorotate.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+export DISPLAY=':0'
+export XAUTHORITY="/home/kruayd/.Xauthority" #what if another user?
+
 rotate="/usr/local/bin/rotate-all.sh"
+rotatep="/usr/local/bin/rotate-pen.sh"
+rotatee="/usr/local/bin/rotate-eraser.sh"
 current="none"
 lastr="none"
+pen="none"
+eraser="none"
 
 accelx=$(find /sys/devices/ -iname *accel_x_raw | grep HID-SENSOR-200073.12)
 accely=$(find /sys/devices/ -iname *accel_y_raw | grep HID-SENSOR-200073.12)
@@ -27,6 +34,15 @@ while true; do
         then
                 current="none"
         fi
+
+	if [[ $pen = "none" ]]
+	then
+		xinput list | grep "CUST0000:00 04F3:2A4B Pen (0)" && pen="detected" ; test $current != "none" && $rotatep $current
+	fi
+	if [[ $eraser = "none" ]]
+	then
+		xinput list | grep "CUST0000:00 04F3:2A4B Eraser (0)" && eraser="detected" ; test $current != "none" && $rotatee $current
+	fi
 
         if [[ $lastr != $current ]]
         then
