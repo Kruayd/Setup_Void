09000f1cd398a6b95648b7fbe9f79b1574b9139f
anon <anon@void-live.fritz.box>
2021-11-03 00:46:07 +0000

    corrected bugs in setup.sh

--- setup.sh
+++ setup.sh
@@ -81,7 +81,7 @@ sed -i -e "/TTYS\=/s/\=.*$/\=8/" /etc/rc.conf
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 # Network Time Protocol
 xbps-install -S ntp
-ln -s /etc/sv/isc-ntpd /var/service/
+ln -s /etc/sv/isc-ntpd /etc/runit/runsvdir/default/
 
 
 #####################################################################
@@ -91,7 +91,7 @@ ln -s /etc/sv/isc-ntpd /var/service/
 
 # Power management
 # acpid
-ln -s /etc/sv/acpid /var/service/ || xbps-install -S acpid && ln -s /etc/sv/acpid /var/service/
+ln -s /etc/sv/acpid /etc/runit/runsvdir/default/ || xbps-install -S acpid && ln -s /etc/sv/acpid /etc/runit/runsvdir/default/
 cp -R $SCRIPT_DIR/etc/acpi /etc/
 # elogind
 xbps-install -S elogind
@@ -99,15 +99,15 @@ sed -i -e "/^Handle/s/^/#/" /etc/elogind/logind.conf
 ln -s /etc/sv/elogind /var/service
 # tlp
 xbps-install -S tlp
-ln -s /etc/sv/tlp /var/service/
+ln -s /etc/sv/tlp /etc/runit/runsvdir/default/
 cp -R $SCRIPT_DIR/etc/tlp.d /etc/
 
 # NetworkManager
 xbps-install -S NetworkManager
-rm -rf /var/service/{dhcpcd,wpa_supplicant,wicd}
-sudo xbps-install -S dbus-elogind dbus-elogind-libs dbus-elogind-x11
-ls /var/service/dbus || ln -s /etc/sv/dbus /var/service/
-ln -s /etc/sv/NetworkManager /var/service/
+rm -rf /etc/runit/runsvdir/default/{dhcpcd,wpa_supplicant,wicd}
+xbps-install -S dbus-elogind dbus-elogind-libs dbus-elogind-x11
+ls /etc/runit/runsvdir/default/dbus || ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
+ln -s /etc/sv/NetworkManager /etc/runit/runsvdir/default/
 
 
 #####################################################################
@@ -122,7 +122,7 @@ ln -s /etc/sv/NetworkManager /var/service/
 xbps-install -S linux-firmware-intel mesa-dri vulkan-loader mesa-vulkan-intel intel-video-accel
 
 # Xorg
-xbps-install xorg
+xbps-install -S xorg
 cp -R $SCRIPT_DIR/etc/X11/xorg.conf.d /etc/X11/
 xbps-remove -F xorg-video-drivers # Intel specific (not sure)
 
@@ -134,17 +134,17 @@ xbps-remove -F xorg-video-drivers # Intel specific (not sure)
 #####################################################################
 
 # Wayland
-xbps-install -S wayland qt5-wayland kwayland xorg-server-wayland
+xbps-install -S wayland qt5-wayland kwayland xorg-server-xwayland
 
 # No bitmap fonts:
 ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
 xbps-reconfigure -f fontconfig
 
 # KDE
-xbps-install -S kde5 kde5-baseapps kdegraphics-thumbnailers ffmpegthumbss accountsservice
-ln -s /etc/sv/sddm /var/service/
+xbps-install -S kde5 kde5-baseapps kdegraphics-thumbnailers ffmpegthumbs accountsservice
+ln -s /etc/sv/sddm /etc/runit/runsvdir/default/
 cp /usr/share/wayland-sessions/plasmawayland.desktop /usr/share/wayland-sessions/plasmawayland.desktop.old
-cp /usr/share/xsession/plasma.desktop /usr/share/xsessions/plasma.desktop.old
+cp /usr/share/xsessions/plasma.desktop /usr/share/xsessions/plasma.desktop.old
 sed -i -e "/^Exec\=/s/\=/\=env QT_QPA_PLATFORM\=wayland-egl MOZ_ENABLE_WAYLAND\=1 /" /usr/share/wayland-sessions/plasmawayland.desktop # we're gonna install firefox later
 
 
@@ -155,15 +155,16 @@ sed -i -e "/^Exec\=/s/\=/\=env QT_QPA_PLATFORM\=wayland-egl MOZ_ENABLE_WAYLAND\=
 #####################################################################
 
 # Multimedia
-xbps-install -S sof-firmware pipewire pipewire-doc pulseeffects libpulseaudio-pipewire alsa-pipewire libjack-pipewire gstreamer1-pipewire libspa-bluetooth
+xbps-install -S sof-firmware pipewire pipewire-doc pulseeffects alsa-pipewire libjack-pipewire gstreamer1-pipewire libspa-bluetooth
+xbps-install -S libpulseaudio-pipewire
 sed -i -e "/^Exec\=/s/MOZ_ENABLE_WAYLAND\=1 /MOZ_ENABLE_WAYLAND\=1 pipewire pipewire-pulse /" /usr/share/wayland-sessions/plasmawayland.desktop
 sed -i -e "/^Exec\=/s/\=/\=pipewire pipewire-pulse /" /usr/share/xsessions/plasma.desktop
 xbps-install -S bluez
-ln -s /etc/sv/bluetoothd /var/service/
+ln -s /etc/sv/bluetoothd /etc/runit/runsvdir/default/
 
 # Printing
 xbps-install -S cups cups-filters
-ln -s /etc/sv/cupsd /var/service/
+ln -s /etc/sv/cupsd /etc/runit/runsvdir/default/
 
 # Useful Kernel modules
 xbps-install -S v4l2loopback
@@ -189,7 +190,7 @@ xbps-install -S wget git make cmake tar gzip ffmpeg
 xbps-install -S iio-sensor-proxy
 cp -R $SCRIPT_DIR/usr/local/bin /usr/local/
 cp -R $SCRIPT_DIR/etc/sv/autorotate /etc/sv/
-ln -s /etc/sv/autorotate /var/service/
+ln -s /etc/sv/autorotate /etc/runit/runsvdir/default/
 # Need to understand how to install maliit keyboard
 # or any other good virtual keyboard
 
